024e62240ccc54cb86169f6a20f8759cc33bbeaa
Gaurav Srivastava <118471686+gauravkrsrivastava@users.noreply.github.com>
2023-05-25 17:03:06 +0530

    Delete ecommercenew-app directory

--- ecommercenew-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images:{
-    domains:['i.dummyjson.com','dummyjson.com']
-  }
-}
-
-module.exports = nextConfig
