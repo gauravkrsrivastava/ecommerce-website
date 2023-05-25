a671cb4b34bf412f87186ef7d4ee20c0c2c5805e
Gaurav Srivastava <118471686+gauravkrsrivastava@users.noreply.github.com>
2023-05-25 17:00:20 +0530

    Add files via upload

---
+++ ecommercenew-app/src/pages/thank-you.js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+function ThankYou(){
+    const router=useRouter();
+    console.log('router',router.query)
+    return(
+        <div>
+            <h1>Thank you for shopping us</h1>
+        </div>
+    )
+}
+export default ThankYou
